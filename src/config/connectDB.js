9b66fba2b929f2c02da00da4b34396e6d849a25d
sagar gupta <sagar8446gupta@gmail.com>
2024-05-31 09:44:45 +0530

    latest chng

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '82.180.143.1',
+    host: '82.180.143.28',
     user: 'u736231290_gameofskilive',
     password: '',
     database: 'u736231290_gameofskilive',
